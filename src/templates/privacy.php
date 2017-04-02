551c4ba110126cbd205cc4f3306c7229a4686c60
Tonda Kozák <kozak.tonda@hotmail.com>
2017-04-02 12:09:21 +0100

    Privacy Policy

---
+++ src/templates/privacy.php
@@ -0,0 +1,14 @@
+<?php
+function generate_privacy() {
+    echo <<<END
+    <h3>What Personal Information do we gather?</h3>
+    <p>Information ONE: when you register you will provide us with your full name and an email address that you will use to login and use our website.
+    <p>Information Two: we also take your full address, your name and your telephone number when you place an order on our e-commerce website.
+    <p>What happens to this personal information: this information is stored on our database.
+    <h3>Automatic information</h3>
+        <p>We receive and store certain types of information whenever you interact on our website. For example, like many websites, we use “cookies” and we obtain information when you access our website. Through this, we will show the product you have recently viewed on our homepage.
+    <h3>What information can I access</h3>
+        <p>Our e-commerce website gives you information about your account and your orders, for a limited purpose of viewing and in certain cases, updating that information. A customer can edit their address details if they want to change that certain detail.
+
+END;
+}
